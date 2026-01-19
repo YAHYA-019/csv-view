2ec4a8b0fa885e3b789e5bb241ecb1b306e98a93
YAHYA-019 <99672717+YAHYA-019@users.noreply.github.com>
2026-01-20 04:48:50 +0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import DataPortal from "./DataPortal";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <DataPortal />
-  </React.StrictMode>
-);
